0e854a79ebdff3d8ab37e3cd33ca287e9d33879d
Yuns <yuns.xie@qq.com>
2024-01-05 23:23:55 +0800

    ✨ feat: fallback scriptDirectory to root path

--- src/helpers/models/common.ts
+++ src/helpers/models/common.ts
@@ -4,18 +4,18 @@ export interface InitializeFastTextModuleOptions
   extends Partial<Pick<EmscriptenModule, 'locateFile'>> {}
 
 /**
- * It will load fasttext.common.wasm from public root directly by default,
+ * If `document.currentScript.src` is falsy value, it will load `fasttext.common.wasm` from public root directly by default,
  *
  * You can download it from https://github.com/yunsii/fasttext.wasm.js/blob/master/src/core/fastText.common.wasm
  *
- * You can also use `locateFile` callback to custom fasttext.common.wasm full path.
+ * You can also use `locateFile` callback to custom `fasttext.common.wasm` full path.
  */
 export async function initializeFastTextModule(
   options?: InitializeFastTextModuleOptions,
 ) {
   globalThis.fastTextModule ??= await fastTextModularized({
     locateFile: (url, scriptDirectory) => {
-      return scriptDirectory + url
+      return (scriptDirectory || '/') + url
     },
     ...options,
   })
