d22dc2d231af9de56fc8aab6581622dacb47dfee
Yuns <yuns.xie@qq.com>
2024-01-05 23:13:49 +0800

    ♻️ chore: comment about common initializeFastTextModule

--- src/helpers/models/common.ts
+++ src/helpers/models/common.ts
@@ -3,6 +3,13 @@ import fastTextModularized from '../../core/fastText.common'
 export interface InitializeFastTextModuleOptions
   extends Partial<Pick<EmscriptenModule, 'locateFile'>> {}
 
+/**
+ * It will load fasttext.common.wasm from public root directly by default,
+ *
+ * You can download it from https://github.com/yunsii/fasttext.wasm.js/blob/master/src/core/fastText.common.wasm
+ *
+ * You can also use `locateFile` callback to custom fasttext.common.wasm full path.
+ */
 export async function initializeFastTextModule(
   options?: InitializeFastTextModuleOptions,
 ) {
